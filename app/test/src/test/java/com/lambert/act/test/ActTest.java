a337e68d9906ca198d6cad871dd6d200b2df9ed1
linzekuan <linzekuan@163.com>
2018-06-14 21:08:09 +0800

    pom

--- app/test/src/test/java/com/lambert/act/test/ActTest.java
+++ app/test/src/test/java/com/lambert/act/test/ActTest.java
@@ -1,6 +1,13 @@
 package com.lambert.act.test;
 
+import org.junit.Test;
+
 import com.lambert.act.test.base.JunitTest;
 
 public class ActTest extends JunitTest {
+	
+	@Test
+	public void test() {
+		
+	}
 }
