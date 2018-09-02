34ca64be7051b8c2c276d4998fbb6334eefaf55e
林泽宽 <linzekuan@163.com>
2018-09-02 22:16:15 +0800

    new

---
+++ app/common/service/integration/src/main/java/com/lambert/act/common/service/integration/App.java
@@ -0,0 +1,13 @@
+package com.lambert.act.common.service.integration;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
